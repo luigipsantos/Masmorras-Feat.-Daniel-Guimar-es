6573041f59388531d65025651c8acccb529145e1
luigipsantos <65872324+luigipsantos@users.noreply.github.com>
2020-12-21 22:59:56 +0000

    Upload

---
+++ CenárioMasmorras/Assets/Scripts/Chase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chase : MonoBehaviour
+{
+    public Transform player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (Vector3.Distance(player.position, this.transform.position) < 3)
+        {
+            Vector3 direction = player.position - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                            Quaternion.LookRotation(direction), 0.5f);
+
+            if (direction.magnitude > 1)
+            {
+                this.transform.Translate(0, 0, 0.05f);
+            }
+
+        }
+
+    }
+
+}
---
+++ CenárioMasmorras/Assets/Scripts/Move.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Classe move.
+public class Move : MonoBehaviour
+{
+
+    float _velocidadeFrente;
+    float _velocidadeTras;
+    float _velocidadeGirar;
+
+    //Metodo Start e executado uma unica vez, quando o script e executado.
+    void Start()
+    {
+        _velocidadeFrente = 6f;
+        _velocidadeTras = 5f;
+        _velocidadeGirar = 200f;
+    }
+
+    //Medodo Update e executado a cada Frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, (_velocidadeFrente * Time.deltaTime));
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, (-_velocidadeTras * Time.deltaTime));
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, (-_velocidadeGirar * Time.deltaTime), 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, (_velocidadeGirar * Time.deltaTime), 0);
+        }
+    }
+}
+
